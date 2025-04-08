a391f3fd659d0b0a00683e0d68e99e5d76f0a084
ramudevasani <devasani.ramu.cse22@itbhu.ac.in>
2025-04-08 23:05:39 +0530

    ..

--- PersistentTrieNode.hpp
+++ PersistentTrieNode.hpp
@@ -1,11 +1,32 @@
 #ifndef PERSISTENTTRIENODE_H
 #define PERSISTENTTRIENODE_H
 
+#include<string>
+#include<unordered_map>
+#include<optional>
+#include<cassert>
+
 class PersistentTrieNode{
 public:
+    PersistentTrieNode() = default;
+
+    PersistentTrieNode* addChild(char c);
+
+    void updateValue(std::string_view newValue = "");
+
+    PersistentTrieNode* getChild(char c);
+
+    bool hasChild() const ;
+
+    bool hasValue() const;
+
+    void removeChild(char c);
+
+    std::optional<std::string> getValue() const ;
 
 private:
-    
+    std::string m_value{};
+    std::unordered_map<char, PersistentTrieNode*>m_child;
 };
 
 #endif
